455506db9c507378db2273a46a89f43285213aac
narcoticfresh <wee@xbe.ch>
2015-09-28 11:06:29 +0200

    remove workerId from action props

--- src/main/java/com/github/libgraviton/workerbase/model/GravitonFileMetadataAction.java
+++ src/main/java/com/github/libgraviton/workerbase/model/GravitonFileMetadataAction.java
@@ -1,15 +1,8 @@
 package com.github.libgraviton.workerbase.model;
 
 public class GravitonFileMetadataAction {
-    public String workerId;
     public String command;
     
-    public String getWorkerId() {
-        return workerId;
-    }
-    public void setWorkerId(String workerId) {
-        this.workerId = workerId;
-    }
     public String getCommand() {
         return command;
     }
