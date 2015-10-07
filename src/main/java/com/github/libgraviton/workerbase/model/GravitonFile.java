5aed5b56b026071e028f06774fe6020047793d0d
Adrian Pauli <Adrian.Pauli@swisscom.com>
2015-10-07 12:01:01 +0200

    Added filtering by link type to GravitonFile

--- src/main/java/com/github/libgraviton/workerbase/model/GravitonFile.java
+++ src/main/java/com/github/libgraviton/workerbase/model/GravitonFile.java
@@ -3,25 +3,44 @@ package com.github.libgraviton.workerbase.model;
 import java.util.ArrayList;
 
 public class GravitonFile {
+
     public String id;
+
     public GravitonFileMetadata metadata;
+
     public ArrayList<GravitonFileLinks> links;
-    
+
+
     public String getId() {
         return id;
     }
+
     public void setId(String id) {
         this.id = id;
     }
+
     public GravitonFileMetadata getMetadata() {
         return metadata;
     }
+
     public void setMetadata(GravitonFileMetadata metadata) {
         this.metadata = metadata;
     }
+
     public ArrayList<GravitonFileLinks> getLinks() {
         return links;
     }
+
+    public ArrayList<GravitonFileLinks> getLinks(String type) {
+        ArrayList<GravitonFileLinks> links = new ArrayList<GravitonFileLinks>();
+        for(GravitonFileLinks link : this.getLinks()) {
+            if(link.getType() == type) {
+                links.add(link);
+            }
+        }
+        return links;
+    }
+
     public void setLinks(ArrayList<GravitonFileLinks> links) {
         this.links = links;
     }
