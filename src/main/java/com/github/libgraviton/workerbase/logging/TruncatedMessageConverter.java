64f4cbe8061c9370d94d50e65b02b123f69930f1
Michael Wegener <michael.wegener@swisscom.com>
2017-05-10 15:11:17 +0200

    add test

--- src/main/java/com/github/libgraviton/workerbase/logging/TruncatedMessageConverter.java
+++ src/main/java/com/github/libgraviton/workerbase/logging/TruncatedMessageConverter.java
@@ -3,6 +3,8 @@ package com.github.libgraviton.workerbase.logging;
 import ch.qos.logback.classic.pattern.ClassicConverter;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 
+import java.util.List;
+
 /**
  * <p>Allows to truncate the message size and add a postfix to mark the message visibly as truncated.</p>
  *
@@ -14,12 +16,12 @@ public class TruncatedMessageConverter extends ClassicConverter {
 
     @Override
     public String convert(ILoggingEvent event) {
-        if(getOptionList().size() != 2) {
+        if(getOptions().size() != 2) {
             throw new IllegalArgumentException("Expected 2 arguments but got " + getOptionList().size() + ".");
         }
 
-        int maxMessageLength = Integer.parseInt(getOptionList().get(0));
-        String postfix = getOptionList().get(1);
+        int maxMessageLength = Integer.parseInt(getOptions().get(0));
+        String postfix = getOptions().get(1);
         String message = event.getFormattedMessage();
 
         if (message.length() >= maxMessageLength) {
@@ -27,5 +29,12 @@ public class TruncatedMessageConverter extends ClassicConverter {
         }
 
         return message;
+    }
+
+    /**
+     * For some reason getOptionList() is protected. With getOptions() it's exposed and testable.
+     */
+    public List<String> getOptions() {
+        return getOptionList();
     }
 }
