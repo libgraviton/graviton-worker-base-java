fe10ec1a80f0833845526509bea03bcb4d6d28dd
narcoticfresh <dn@xbe.ch>
2021-02-02 11:54:33 +0100

    remove ImpersonatedGatewayRequest.java - move it to RequestExecutor level

--- src/main/java/com/github/libgraviton/workerbase/auth/exception/CannotProcessAuth.java
+++
@@ -1,9 +0,0 @@
-package com.github.libgraviton.workerbase.auth.exception;
-
-public class CannotProcessAuth extends Exception {
-
-    public CannotProcessAuth(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-}
