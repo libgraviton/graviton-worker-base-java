6783409751f1b62e1c6c01342597d8aa43e2e927
Dario Nuevo <wee@xbe.ch>
2022-12-07 15:20:11 +0100

    fix

--- src/main/java/com/github/libgraviton/workerbase/AsyncQueueWorkerAbstract.java
+++ src/main/java/com/github/libgraviton/workerbase/AsyncQueueWorkerAbstract.java
@@ -1,10 +1,10 @@
 package com.github.libgraviton.workerbase;
 
-import com.github.libgraviton.workerbase.gdk.GravitonApi;
+import com.github.libgraviton.workerbase.gdk.GravitonAuthApi;
 import com.github.libgraviton.workerbase.model.QueueEvent;
 
 public abstract class AsyncQueueWorkerAbstract extends QueueWorkerAbstract implements AsyncQueueWorkerInterface {
     @Override
-    final public void handleRequest(QueueEvent body, GravitonApi gravitonApi) {
+    final public void handleRequest(QueueEvent body, GravitonAuthApi gravitonApi) {
     }
 }
