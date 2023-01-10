0257a87762b520fe40fd8c9cb02ffbf559453c93
Dario Nuevo <wee@xbe.ch>
2023-01-10 11:05:06 +0100

    use a class scan cache via a file generated by the parent pom

--- src/main/java/com/github/libgraviton/workerbase/helper/DependencyInjection.java
+++ src/main/java/com/github/libgraviton/workerbase/helper/DependencyInjection.java
@@ -10,7 +10,7 @@ import io.github.classgraph.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 public class DependencyInjection {
@@ -18,20 +18,40 @@ public class DependencyInjection {
     private static Injector injector;
 
     public static final String scanClass = "com.github.libgraviton";
+    public static final String cacheFileName = "graviton-di-class-scan-cache";
 
     private static final Logger LOG = LoggerFactory.getLogger(DependencyInjection.class);
 
     private static final HashMap<Class<?>, Object> instanceOverrides = new HashMap<>();
+    private static Map<String, List<String>> classScanCache = null;
 
     public static void init() {
         init(List.of());
     }
 
     private static Set<Class<?>> doClassScan(Class<?> interestedAnnotation) {
+        return doClassScan(interestedAnnotation, true);
+    }
+
+    private static Set<Class<?>> doClassScan(Class<?> interestedAnnotation, boolean useCache) {
         final Set<Class<?>> clazzez = new HashSet<>();
 
+        if (useCache) {
+            loadClassScanCache();
+            if (classScanCache != null && classScanCache.containsKey(interestedAnnotation.getName())) {
+                for (String clazzName : classScanCache.get(interestedAnnotation.getName())) {
+                    try {
+                        clazzez.add(DependencyInjection.class.getClassLoader().loadClass(clazzName));
+                    } catch (Throwable t) {
+                        LOG.error("Unable to load cached class named '{}'", clazzName, t);
+                    }
+                }
+
+                return clazzez;
+            }
+        }
+
         ClassGraph classScan = new ClassGraph()
-                .acceptClasses()
                 .enableAnnotationInfo()
                 .acceptPackages(scanClass);
 
@@ -110,6 +130,48 @@ public class DependencyInjection {
             return (T) instanceOverrides.get(clazz);
         }
         return injector.getInstance(clazz);
+    }
+
+    public static void cacheAllClassScanResults(String path) throws IOException {
+        Map<String, List<String>> classNames = new HashMap<>();
+
+        for (Class<?> clazz : List.of(GravitonWorker.class, GravitonWorkerDiScan.class)) {
+            classNames.putIfAbsent(
+                    clazz.getName(),
+                    doClassScan(clazz, false).stream().map(Class::getName).toList()
+            );
+        }
+
+        FileOutputStream file = new FileOutputStream(path + "/" + cacheFileName);
+        ObjectOutputStream out = new ObjectOutputStream(file);
+
+        out.writeObject(classNames);
+
+        out.close();
+        file.close();
+    }
+
+    private static void loadClassScanCache() {
+        // already loaded?
+        if (classScanCache != null) {
+            return;
+        }
+
+        try {
+            InputStream is = DependencyInjection.class.getClassLoader().getResourceAsStream(cacheFileName);
+
+            if (is == null) {
+                return;
+            }
+
+            ObjectInputStream in = new ObjectInputStream(is);
+            classScanCache = (Map) in.readObject();
+            in.close();
+
+            LOG.info("Successfully loaded DI class scan cache from '{}'", cacheFileName);
+        } catch (Throwable t) {
+            // it's ok!
+        }
     }
 
     public static Set<Class<?>> getWorkerClasses() {
