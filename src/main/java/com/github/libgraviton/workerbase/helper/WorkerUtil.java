5f23ddd5959632f79b72c2ad77b19f135397116b
Dario Nuevo <wee@xbe.ch>
2023-03-02 17:12:17 +0100

    put all durations here..

--- src/main/java/com/github/libgraviton/workerbase/helper/WorkerUtil.java
+++ src/main/java/com/github/libgraviton/workerbase/helper/WorkerUtil.java
@@ -15,6 +15,8 @@ import org.apache.commons.io.IOUtils;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.List;
 
 /**
  * <p>WorkerUtil class.</p>
@@ -25,12 +27,40 @@ import java.nio.charset.StandardCharsets;
  */
 public class WorkerUtil {
 
+    private final static Duration[] metricDurations = List.of(
+            Duration.ofMillis(250),
+            Duration.ofMillis(500),
+            Duration.ofSeconds(1),
+            Duration.ofSeconds(2),
+            Duration.ofSeconds(3),
+            Duration.ofSeconds(4),
+            Duration.ofSeconds(5),
+            Duration.ofSeconds(10),
+            Duration.ofSeconds(15),
+            Duration.ofSeconds(20),
+            Duration.ofSeconds(25),
+            Duration.ofSeconds(30),
+            Duration.ofSeconds(35),
+            Duration.ofSeconds(40),
+            Duration.ofSeconds(45),
+            Duration.ofSeconds(50),
+            Duration.ofSeconds(55),
+            Duration.ofSeconds(60),
+            Duration.ofSeconds(120),
+            Duration.ofSeconds(240),
+            Duration.ofSeconds(600)
+    ).toArray(Duration[]::new);
+
     public static String getWorkerBaseVersion() {
         try {
             return IOUtils.resourceToString("worker-base-version", StandardCharsets.UTF_8, WorkerUtil.class.getClassLoader());
         } catch (Throwable t) {
             return "(UNKNOWN)";
         }
+    }
+
+    public static Duration[] getTimeMetricsDurations() {
+        return metricDurations;
     }
 
     // create a trust manager that does not validate certificate chains
