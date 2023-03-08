1d0012e575137979f3f5a779455c165d6acf8503
Dario Nuevo <wee@xbe.ch>
2023-03-08 10:52:13 +0100

    we should use this..

--- src/main/java/com/github/libgraviton/workerbase/gdk/requestexecutor/auth/Authenticator.java
+++ src/main/java/com/github/libgraviton/workerbase/gdk/requestexecutor/auth/Authenticator.java
@@ -3,10 +3,8 @@ package com.github.libgraviton.workerbase.gdk.requestexecutor.auth;
 import com.github.libgraviton.workerbase.gdk.api.Request;
 import com.github.libgraviton.workerbase.gdk.requestexecutor.exception.AuthenticatorException;
 
-public interface Authenticator {
+public interface Authenticator extends AutoCloseable {
 
     Request onRequest(Request request) throws AuthenticatorException;
-
-    void onClose() throws AuthenticatorException;
 
 }
